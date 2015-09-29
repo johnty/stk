c145a078ea448fc29e65c622ac0823b0a3be5744
johnty <johntywang@gmail.com>
2015-09-29 18:10:26 -0400

    backup

--- projects/examples/doppler.cpp
+++ projects/examples/doppler.cpp
@@ -1,6 +1,73 @@
-// doppler.cpp MUMT618 Assignemt 3
+// doppler.cpp
+// MUMT618 Assignemt 3
 // 30 Sep 2015
 // Johnty Wang (johnty.wang@mail.mcgill.ca)
+
+// Implements a simple 1D doppler effect for a given input .wav file as well as a few input parameters
+// that are described if the application is executed without any arguments.
+
+//***** To compile: add following to stk/projects/examples/MakeFile.in
+//      and then run root ./configure followed by "make doppler" in examples folder.
+//
+//doppler: doppler.cpp Stk.o DelayL.o FileRead.o FileWvIn.o FileWvOut.o FileWrite.o FileLoop.o
+//  $(CC) $(LDFLAGS) $(CFLAGS) $(DEFS) -o doppler doppler.cpp $(OBJECT_PATH)/Stk.o $(OBJECT_PATH)/DelayL.o $(OBJECT_PATH)/FileRead.o $(OBJECT_PATH)/FileWvIn.o $(OBJECT_PATH)/FileWrite.o $(OBJECT_PATH)/FileWvOut.o $(OBJECT_PATH)/FileLoop.o $(OBJECT_PATH)/RtAudio.o $(LIBRARY)
+//
+//*****
+
+//
+// Usage: ./doppler [spd src (m/s)] [spd recv (m/s)] [distX (m)] [distY (m)] [input file (wav)] [output file (wav)] [mix]
+//
+//    - spd src/recv: speed of source and receivers, in m/s. + if towards the other.
+//
+//    - dist: initial distance between the two
+//
+//    - input file: input file to be processed. will be looped continuously throughout duration of simulation
+//
+//    - output file: output file to be written
+//
+//    - mix: whether to do "half delay length" mixing or not, for smoothing discontinuity when read pointer
+//      overtakes the write pointer.
+//
+//
+// Sample input:
+// ./doppler 10 5 120 10 siren.wav siren_doppler.wav 0
+// will create a source and receiver travelling at 10m/s and -5m/s respectively
+// from a distance of distX=120m and distY. The simulation will end when they are dixtX=120m apart after crossing each other.
+// in our internal model, we set the starting location of the source as 0, and receiver in the positive direction
+//
+//
+//    SRC -> (10m/s)
+//    |                                              |
+//    |                                              |
+//    |                                              |
+//    |                                              | (distY)
+//    |                                              |
+//    |                                              |
+//     ----------------- 120m -----------------------
+//                       (distX)           (5m/s) <- RECEIVER
+//               fig 1.
+//
+//
+//                                                                 SRC -> (10m/s)
+//                  |                                              |
+//                  |                                              |
+//                  |                                              |
+//                  |                                              | (distY)
+//                  |                                              |
+//                  |                                              |
+//                  ----------------- 120m -----------------------
+//   (5m/s) <- RECEIVER                    (distX)
+//
+//               fig 2.
+//
+//
+//   based on the above configuration, the simulation will take 2*distX/(speed of source + speed of receiver)
+//   if the distance between the two are diverging (or constant) as an initial condition, we clamp simulation to 5 secs
+//   since the crossover point will never be reached, and we should get no frequency shift.
+//
+//   Possible improvements:
+//      - extend kinematic model to arbiturary directions
+//
 
 #include "FileWvIn.h"
 #include "FileLoop.h"
@@ -9,7 +76,7 @@
 #include <cstdlib>
 
 double SAMPLE_RATE = 44100.0;
-unsigned int DELAY_SIZE = 1024;
+unsigned int DELAY_SIZE = 1024; //for setting the delay line length, used for tapping into the delay line for (optional) mixing
 double SPEED_OF_SOUND = 343.0;
 
 using namespace stk;
@@ -18,120 +85,153 @@ int main(int argc, char *argv[])
 {
   double velSource;
   double velReciver;
-  double dist;
-  double velRelative;
+  double distX;
+  double distY;
+  double velRelativeX;
   double totalTime;
   unsigned long numSamples;
   
-  if ( argc < 6 || argc > 7 ) {
+  if ( argc < 7 || argc > 8 ) {
     std::cout<<"\n\n\n****Doppler shift example*****\n\n";
     std::cout<<"Will produce output assuming source is travelling towards\n";
     std::cout<<"and then past receiver in straight line\n\n";
     std::cout<<"Will start simulation at d0=-D and end at d1=+D\n";
     std::cout<<"Simulation time = 2D/(relative velocity)\n";
     std::cout<<"input will be looped for duration of simulation\n\n";
-    std::cout<< "usage: doppler [spd src (m/s)] [spd recv (m/s)] [dist (m)] [input file (wav)] [0/1: no mix/mix]\n";
-    std::cout<< "NOTE1: for speeds, + is towards, - is away from\n";
-    std::cout<< "NOTE2: if source and receiver are diverging, fix simulation duration to 5s\n";
+    std::cout<< "usage: doppler [spd src (m/s)] [spd recv (m/s)] [distX (m)] [distY (m)] [input file (wav)] [output file (wav)] [0/1: no mix/mix]\n";
+    std::cout<< "NOTE: if source and receiver are diverging, fix simulation duration to 5s\n";
     return 0;
   }
   else {
     std::cout<<"INPUT OK...\n parsed values:";
     velSource = atof(argv[1]);
     velReciver = atof(argv[2]);
-    dist = atof(argv[3]);
-    std::cout<<"velSource=" << velSource << " velReciver=" << velReciver << " dist=" << dist << " if=" << argv[4] << "\n\n";
+    distX = atof(argv[3]);
+    distY = atof(argv[4]);
+    //visual check to make sure input was parsed properly
+    std::cout<<"velSource=" << velSource << " velReciver=" << velReciver << " distX=" << distX << " distY=" <<distY << " if=" << argv[5] << "\n\n";
     
-    velRelative = velSource+velReciver;
-    totalTime = dist/velRelative;
+    velRelativeX = velSource-velReciver;
+    
+    double distAbs = sqrt(distX*distX + distY*distY);
+    
+    totalTime = 2*distX/velRelativeX; //total simulation time, if not diverging/constant
+    
     //do some basic input checking:
-    if ((velSource + velReciver) <= 0) {
-      std::cout<<"clamping time to 5 seconds";
+    if ((velSource - velReciver) <= 0) {
+      //this is for the "not so interesting" case where
+      //the source and receiver will never "cross"
+      // will just have a boring, constant frequency...
+      //so we force a fixed simulation time here.
+      std::cout<<"DIVERGING! clamping time to 5 seconds\n";
       totalTime = 5.0;
     }
     numSamples = totalTime * SAMPLE_RATE;
-    std::cout<<"relative speed = " << velRelative<<"; simulation time = " << totalTime<< "s; numSamples = "<< numSamples<<"\n";
+    std::cout<<"relative speedX = " << velRelativeX<<"; simulation time = " << totalTime<< "s; numSamples = "<< numSamples<<"\n";
   }
-  // Set the global sample rate before creating class instances.
+  
   Stk::setSampleRate( SAMPLE_RATE );
-
+  
+  //file i/o
   FileWvOut outputFile;
   FileLoop inputLoop;
 
-  // Define and open a 16-bit, single-channel WAV
   try {
-	  outputFile.openFile( "doppler_out.wav", 1, FileWrite::FILE_WAV, Stk::STK_SINT16 );
-    inputLoop.openFile(argv[4]);
-    std::cout<<"opened input file samples = " << inputLoop.getSize() <<"\n";
+	  outputFile.openFile( argv[6], 1, FileWrite::FILE_WAV, Stk::STK_SINT16 );
+    inputLoop.openFile(argv[5]);
   }
   catch (StkError &) {
     exit( 1 );
   }
   
-  //the doppler effect's frequency shift is implemented using a delay line that is written incrementally
-  // and read using a variable delay.
-  // Since the read pointer might catch up/wrap around the write pointer,
-  // We need to do some averaging between the current read position and half-delayline-length position
-  // to do so we can tap the delay at 1/2 the length
-  // and keep track of a "mixing" variable that goes towards zero as the read ptr approaches the write ptr
+  //the doppler effect's frequency shift is implemented using a delay line whose write
+  // pointer is incremented sample by sample and read using a variable delay.
   DelayL delay(1.0, DELAY_SIZE);
-  
 
   //initialize: since read pointer can increment faster than write pointer, we should
   // fill up the delay line with initial values for continuity's sake...
-  
-  // since our input is a loop, it doesn't matter too much where the "starting" point
+    // since our input is a loop, it doesn't matter too much where the "starting" point
   // in the audio sample is...
-  
   for (int i=0; i<DELAY_SIZE; i++) {
     delay.tick(inputLoop.tick());
   }
   
   stk::StkFrames frames( numSamples, 1);
-  //go through the buffer sample by sample
-  double read_ptr = 0;
-  unsigned int write_ptr = 0;
-  //numSamples = 100;
+  double readPtr = 0;
+  unsigned int writePtr = 0;
   double g=0;
-  int numWrap = 0;
+
+  // set initial locations
+  double xSrcCurr = 0, xSrcPrev = 0;
+  double xRcvCurr = distX, xRcvPrev = distX;
+  double dT = totalTime/numSamples;
+  
   for (int i=0; i<numSamples; i++) {
-    //0.) write current value into delay line
     
-    //get current relative speed, conver into delay amount and update delay line
-    //note: we have a small truncation error for the mixing value here
-    if (i < numSamples/2)
-      g = - velRelative/SPEED_OF_SOUND; // "growth parameter"
-    else
-      g = + velRelative/SPEED_OF_SOUND;
+    //basic kinematic model for estimating current relative speed:
+    double timeElapsed = (double)totalTime*i/numSamples;
+    xSrcCurr+=dT*velSource;
+    xRcvCurr+=dT*velReciver;
     
-    read_ptr+=g;
+    //distY is constant!
+    
+    //velRelative = ( sqrt((xSrcCurr-xSrcPrev)*(xSrcCurr-xSrcPrev)+distY*distY) +
+    //               sqrt((xRcvPrev-xRcvCurr)*(-xRcvCurr)+distY*distY) ) / dT;
+    
+    double velRelative = ( (xRcvCurr-xSrcCurr) - (xRcvPrev-xSrcPrev) ) / dT;
+    if (xRcvCurr < xSrcCurr)
+      velRelative = -velRelative; //bit of a hack for the sign here...
+    
+        std::cout <<xSrcCurr<<"  "<<xRcvCurr<< " "<< velRelative <<"\n";
+    
+    //std::cout << velRelative <<"\n";
+    
+    xSrcPrev = xSrcCurr;
+    xRcvPrev = xRcvCurr;
+    
+    //if (i < numSamples/2)
+    //  g = - velRelative/SPEED_OF_SOUND; // "growth parameter"
+    //else
+    g = + velRelative/SPEED_OF_SOUND;
+    readPtr+=g; //update read pointer with variable delay
 
     //wraparound checks:
-    if (read_ptr < 0)
-      read_ptr+=DELAY_SIZE;
-    if (read_ptr>DELAY_SIZE-1)
-      read_ptr-=DELAY_SIZE;
+    if (readPtr < 0)
+      readPtr+=DELAY_SIZE;
+    if (readPtr>DELAY_SIZE-1)
+      readPtr-=DELAY_SIZE;
     
     //write pointer simply increments...
-    if (write_ptr>DELAY_SIZE)
-      write_ptr-=DELAY_SIZE;
-
-    write_ptr++;
+    writePtr++;
+    if (writePtr>DELAY_SIZE)
+      writePtr-=DELAY_SIZE;
     
-    delay.setDelay(1+read_ptr);
-    double mix = ( (int)(read_ptr-write_ptr) % (DELAY_SIZE) ) / (double) DELAY_SIZE ;
-    if (atoi(argv[5]) == 0)
-      mix = 1; // don't mix; otherwise, use the mixing from half delay line away...
+    //where the magic happens: the variable delay creates the frequency shift
+    delay.setDelay(1+readPtr);
+    
+    //User Selectable switching of the "Mixing": gets rid of discontinuities
+    // when the read pointer crosses the write location by
+    // linearly mixing between half a delay line away.
+    // maximum amplitude is respected, but at the cost
+    // of additional artifacts. better mixing techniques could reduce these...
+    double mix = ( (int)(readPtr-writePtr) % (DELAY_SIZE) ) / (double) DELAY_SIZE ;
+    if (atoi(argv[7]) == 0)
+      mix = 1; // overrides the mixing calculation. i.e. no mixing.
+    
+    //update output sample
     float outsample = mix*delay.tick(inputLoop.tick());
+    
+    //apply mixing (if any)
     outsample += (1-mix)*delay.tapOut(DELAY_SIZE/2);
+
+    //put into frames
     frames[i] = outsample;
+    //std::cout<<"tE = " << timeElapsed<<"\n";
   }
-  std::cout<<"numWrap = " << numWrap<<"\n";
+  
 
-  //dump all frames into outputFile file. (could have incrementally done it directly above...)
-  outputFile.tick( frames );
-
-
+  //dump frames into outputFile file. (could have incrementally done it directly above...)
+  outputFile.tick(frames);
 
   return 0;
 }
